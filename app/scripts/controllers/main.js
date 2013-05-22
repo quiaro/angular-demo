b532d2fc85a2fa62c0ce3ac2a6c066872bc61417
quiaro <david.quiros@rivetlogic.com>
2013-05-22 12:21:35 -0600

    Default Yeoman project setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dialogsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
