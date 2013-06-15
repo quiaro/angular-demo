d357c12dcf00717b81b94004fa194cfedf7cf78d
quiaro <david.quiros@rivetlogic.com>
2013-06-15 09:28:49 -0600

    Add install file

---
+++ install.sh
@@ -0,0 +1,5 @@
+# This install file will :
+# - Install Grunt (if not installed)
+# - Install Bower (if not installed)
+# - Fetch all package (environment) dependencies 
+# - Fetch all component (project) dependencies
