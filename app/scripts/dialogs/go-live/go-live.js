ddd526c2ea3f3f82de34654c51d45eb96f077085
quiaro <david.quiros@rivetlogic.com>
2013-06-17 22:19:05 -0600

    Lint JS files

--- app/scripts/dialogs/go-live/go-live.js
+++ app/scripts/dialogs/go-live/go-live.js
@@ -1,15 +1,17 @@
+'use strict';
+
 angular.module('go-live.dialog', ['ui.bootstrap.dialog'])
 	.factory('goLiveDialog', function($dialog) {
 
-		var opts = {
-			backdrop: true,
-			keyboard: true,
-			backdropClick: true,
-			backdropFade: true,
-			templateUrl: 'scripts/dialogs/go-live/go-live.tpl.html',
-			controller: 'DialogsCtrl'
-		};
+	var opts = {
+		backdrop: true,
+		keyboard: true,
+		backdropClick: true,
+		backdropFade: true,
+		templateUrl: 'scripts/dialogs/go-live/go-live.tpl.html',
+		controller: 'DialogsCtrl'
+	};
 
-		return $dialog.dialog(opts);
+	return $dialog.dialog(opts);
 
 });
