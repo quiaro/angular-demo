89bdbf7dfce6b492df68fd424fda3b85096aac58
quiaro <david.quiros@rivetlogic.com>
2013-06-22 16:15:30 -0600

    Lint code

--- app/scripts/common/services/repo.js
+++ app/scripts/common/services/repo.js
@@ -3,15 +3,14 @@
 angular.module('services.repo', [])
 	.service('repo', function($http) {
 
-	var API, 
-		api = 'repo',
+	var api = 'repo',
 		sitename = 'pebbles';
 
 	// Common function
 	function getServiceURL(api, method, sitename, searchStr) {
 		var res = '/api/0.1/' + api + '/' + method + '/' + sitename + ((!searchStr) ? '' : '?' + searchStr);
 		return res;
-	};
+	}
 
 	function list(filtersObj) {
 
@@ -24,20 +23,21 @@ angular.module('services.repo', [])
 		}
 		url = getServiceURL(api, 'list', sitename, searchStr);
 		return $http.get(url);
-	};
+	}
 
 	function read(item, version) {
 
 		var searchStr, url;
-		
+
 		searchStr = 'item=' + item + 'version=' + version;
 		url = getServiceURL(api, 'read', sitename, searchStr);
 
 		return $http.get(url);
 	}
 
-	return API = {
+	// expose the API to the user
+	return {
 		list: list,
 		read: read
-	}
+	};
 });
