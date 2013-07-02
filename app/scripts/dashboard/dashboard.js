c1777878e683d15c3e6e0af78511444c604f90b9
quiaro <david.quiros@rivetlogic.com>
2013-07-02 12:15:05 -0400

    Inject $window to dashboard controller

--- app/scripts/dashboard/dashboard.js
+++ app/scripts/dashboard/dashboard.js
@@ -2,13 +2,13 @@
 
 angular.module('dashboard', ['dialogs', 'services.repo'])
 
-    .controller('DashboardCtrl', ['$scope', 'repo', function($scope, repo) {
+    .controller('DashboardCtrl', ['$scope', '$window', 'repo', function($scope, $window, repo) {
 
     // Get Recent Activity data    
     repo.list().success(function(data) {
         $scope.recentActivity = data;
     }).error(function() {
-        alert('Error: call to repo.list service failed');
+        $window.alert('Error: call to repo.list service failed');
     });
 
 }]);
