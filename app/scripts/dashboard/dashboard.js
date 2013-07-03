a496799fb4c253fc9cb2f0880f45d51856ba74cf
quiaro <david.quiros@rivetlogic.com>
2013-07-03 11:08:25 -0400

    Test asynchronous loading of recent activity items

--- app/scripts/dashboard/dashboard.js
+++ app/scripts/dashboard/dashboard.js
@@ -4,11 +4,13 @@ angular.module('dashboard', ['dialogs', 'services.repo'])
 
   .controller('DashboardCtrl', ['$scope', '$window', 'repo', function($scope, $window, repo) {
 
-    // Get Recent Activity data    
-    repo.list().success(function(data) {
-        $scope.recentActivity = data;
-    }).error(function() {
-        $window.alert('Unable to retrieve data for recent activity. Please try reloading or contact your system administrator.');
-    });
+    // Get Recent Activity data
+		this.getRecentActivity = function getRecentActivity () {
+			repo.list().success(function(data) {
+				$scope.recentActivity = data;
+			}).error(function() {
+				$window.alert('Unable to retrieve data for recent activity. Please try reloading or contact your system administrator.');
+			});
+		};
 
 	}]);
--- test/spec/dashboard/dashboard.js
+++ test/spec/dashboard/dashboard.js
@@ -2,22 +2,46 @@
 
 describe('Controller: DashboardCtrl', function () {
 
+  var DashboardCtrl, controller, scope, httpBackend, params;
+
   // load the controller's module
   beforeEach(module('dashboard'));
 
-  var DashboardCtrl,
-      scope;
+  beforeEach(inject(function($controller, $httpBackend, $http, $rootScope) {
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, repo) {
+    controller = $controller;
     scope = $rootScope.$new();
-    DashboardCtrl = $controller('DashboardCtrl', {
+    httpBackend = $httpBackend;
+    params = {
       $scope: scope,
-      repo: repo
-    });
+      $window: jasmine.createSpyObj('$window', ['alert']), // overrides $window with a mock version so window.alert() will not block the test runner with a real alert box
+      repo: {
+        list: function list() {
+          return $http.get('/url/to/activity/items');
+        }
+      }
+    };
+    DashboardCtrl = controller('DashboardCtrl', params);
   }));
 
-  // it('should attach an item list to the scope', function () {
-  //   expect(scope.recentActivity.length).toBe(3);
-  // });
+  afterEach(function() {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should attach the list of recent activity items to the scope when the call to repo.list() is successful', function () {
+    httpBackend.expectGET('/url/to/activity/items').respond(200, ['a']);
+    DashboardCtrl.getRecentActivity();
+    httpBackend.flush();    
+    expect(scope.recentActivity).toEqual(['a']);
+  });
+
+  it('should display an error message when the call to repo.list() fails', function () {
+    httpBackend.expectGET('/url/to/activity/items').respond(500);
+    DashboardCtrl.getRecentActivity();
+    httpBackend.flush();
+    expect(params.$window.alert).toHaveBeenCalled();
+    expect(scope.recentActivity).toBeUndefined();
+  });
+
 });
